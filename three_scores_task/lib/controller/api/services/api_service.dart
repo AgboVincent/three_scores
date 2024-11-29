8a6cc451335186443bbfadb44b7ef53c67a75226
AgboVincent <agbovincent100@gmail.com>
2024-11-29 21:36:22 +0100

    added video player ability

--- three_scores_task/lib/controller/api/services/api_service.dart
+++ three_scores_task/lib/controller/api/services/api_service.dart
@@ -4,9 +4,7 @@
 
 
 import 'dart:convert';
-
 import 'package:flutter/services.dart';
-import 'package:three_scores_task/model/video_highlights.dart';
 
 class ApiService {
   Future<Map<String, dynamic>> fetchHighlightsFromJson(String filename) async {
